0e392e10a5740bed9e492141b4b16b0fc70c7603
Jon Leopard <jontron2000@gmail.com>
2018-06-27 15:44:46 +0200

    Major update

--- src/index.js
+++ src/index.js
@@ -1,11 +1,52 @@
 import dotenv from 'dotenv';
+import http from 'http';
+import cors from 'cors';
+import morgan from 'morgan';
+import bodyParser from 'body-parser';
 import express from 'express';
+import initializeDb from './db';
+import middleware from './middleware';
+import api from './api';
+import config from './config.json';
 
-const app = express();
-const port = process.env.PORT || 5000;
+dotenv.config();
+
+let app = express();
+app.server = http.createServer(app);
+
+// logger
+app.use(morgan('dev'));
+
+// 3rd party middleware
+app.use(
+  cors({
+    exposedHeaders: config.corsHeaders,
+  })
+);
+
+app.use(
+  bodyParser.json({
+    limit: config.bodyLimit,
+  })
+);
 
 app.get('/api/hello', (req, res) => {
   res.send({ express: 'Hello From Express' });
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+// connec to db
+initializeDb(db => {
+  // internal middleware
+  app.use(middleware({ config, db }));
+
+  // api router
+  app.use('/api', api({ config, db }));
+
+  app.server.listen(process.env.PORT || config.port, () => {
+    console.log(`Started on port ${app.server.address().port}`);
+  });
+});
+
+console.log(`Pulled from dotenv: ${process.env.SUPER_SECRET_PASS}`);
+
+export default app;
